f82f19b95d2ee4f48b55c345515fe5083811612b
adevvvv <adevvv@bk.ru>
2024-04-03 10:52:28 +0300

    feat: инициализировать проект и добавить в gradle зависимости (spring web, jpa, postgresql, lombok)

---
+++ src/main/java/com/example/it_platform/ItPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItPlatformApplication.class, args);
+	}
+
+}
