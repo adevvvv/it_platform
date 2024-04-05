07dc4268cb6024d4f14ace3f537d83c40c901193
adevvvv <adevvv@bk.ru>
2024-04-05 13:08:15 +0300

    feat: добавить контроллеры для страниц с дорожными картами

--- src/main/java/com/example/it_platform/controllers/RoadmapController.java
+++ src/main/java/com/example/it_platform/controllers/RoadmapController.java
@@ -8,4 +8,54 @@ public class RoadmapController {
     public String roadmap () {
         return "roadmap";
     }
+
+    @GetMapping("/roadmap/intellij")
+    public String roadmapIntellij () {
+        return "roadmap_intellij";
+    }
+
+    @GetMapping("/roadmap/java")
+    public String roadmapJava () {
+        return "roadmap_java";
+    }
+
+    @GetMapping("/roadmap/postgresql")
+    public String roadmapPostgresql () {
+        return "roadmap_postgresql";
+    }
+
+    @GetMapping("/roadmap/redis")
+    public String roadmapRedis () {
+        return "roadmap_redis";
+    }
+
+    @GetMapping("/roadmap/spring")
+    public String roadmapSpring () {
+        return "roadmap_spring";
+    }
+
+    @GetMapping("/roadmap/testng")
+    public String roadmapTestng () {
+        return "roadmap_testng";
+    }
+
+    @GetMapping("/roadmap/docker")
+    public String roadmapDocker () {
+        return "roadmap_docker";
+    }
+
+    @GetMapping("/roadmap/kafka")
+    public String roadmapKafka () {
+        return "roadmap_kafka";
+    }
+
+    @GetMapping("/roadmap/git")
+    public String roadmapGit () {
+        return "roadmap_git";
+    }
+
+    @GetMapping("/roadmap/linux")
+    public String roadmapLinux () {
+        return "roadmap_linux";
+    }
 }
